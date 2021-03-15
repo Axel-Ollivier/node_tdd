91007d39f05d5a3c4d3bf22d06d1488ccfc14fcc
Axel-Ollivier <aimericollivier@gmail.com>
2021-03-15 21:00:08 +0100

    ignore cleanDb

--- spec/helpers/cleanDb.js
+++ spec/helpers/cleanDb.js
@@ -1,10 +1,7 @@
 const cleanDb = async (db) => {
-    await db.Author.destroy({truncate: {cascade: true}});
-    await db.Post.destroy({truncate: {cascade: true}});
-    await db.Attendee.destroy({truncate: {cascade: true}});
-    await db.Booking.destroy({truncate: {cascade: true}});
-    await db.Company.destroy({truncate: {cascade: true}});
-    await db.User.destroy({truncate: {cascade: true}});
-    await db.Account.destroy({truncate: {cascade: true}});
+    await db.Author.truncate({ cascade: true });
+    await db.Post.truncate({ cascade: true });
+    await db.Attendee.truncate({ cascade: true });
+    await db.Booking.truncate({ cascade: true });
 }
 module.exports = cleanDb
