63c0b39e273aca26e63dc8fdff12e71972741f87
Axel-Ollivier <aimericollivier@gmail.com>
2021-03-18 09:35:48 +0100

    account/signin

--- app/api/account.js
+++ app/api/account.js
@@ -1,8 +1,44 @@
+const config = require("../../config/config");
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcryptjs");
+
 module.exports = (app, db) => {
     app.post('/account/signin', async (req, res) => {
         return true
     })
     app.post('/account/signup', async (req, res) => {
-        return true
+        await db.Account.findOne({
+            email: req.body.data.attributes.email,
+        }).then((result) => {
+            if (!result) {
+                return res.status(404).send({message: "Account Not found."});
+            }
+
+            var passwordIsValid = bcrypt.compareSync(
+                req.body.accountPassword,
+                result.accountPassword
+            );
+
+            if (!passwordIsValid) {
+                return res.status(401).send({
+                    accessToken: null,
+                    message: "Invalid Password!"
+                });
+            }
+
+            var token = jwt.sign({id: result.id}, config.secret, {
+                expiresIn: 86400 // 24 hours
+            });
+
+            return res.res.status(200).send({
+                "data": {
+                    "attributes": {
+                        "id": result._id,
+                        "email": result.email,
+                        "accessToken": token
+                    }
+                }
+            });
+        }).catch((e) => console.log(e.message))
     })
 }
--- spec/helpers/cleanDb.js
+++
@@ -1,9 +0,0 @@
-const cleanDb = async (db) => {
-    await db.Author.truncate({cascade: true});
-    await db.Post.truncate({cascade: true});
-    await db.Attendee.truncate({cascade: true});
-    await db.Booking.truncate({cascade: true});
-    await db.User.truncate({cascade: true});
-    await db.Company.truncate({cascade: true});
-}
-module.exports = cleanDb
